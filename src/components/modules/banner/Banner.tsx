4614d1282e46696fbc7f9fae26b3123c77997492
hyunjeen <dev.hyunjeen@gmail.com>
2023-06-02 20:45:23 +0900

    style: Banner 컴포넌트 사이즈 수정 및 index 페이지 레이아웃 수정

--- src/components/modules/banner/Banner.tsx
+++ src/components/modules/banner/Banner.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
-import bannerImage from "../../../../public/banner.png";
+import bannerImage from "/public/banner.png";
 import BannerInfo from "@/components/modules/banner/BannerInfo";
 
 function Banner() {
   return (
-    <section className={`overflow-hidden relative rounded cursor-pointer`}>
+    <section
+      className={`flex overflow-hidden relative rounded cursor-pointer basis-[78.125%]`}
+    >
       <Image
         src={bannerImage}
         alt={"배너이미지"}
-        className={`transition ease-in-out hover:scale-105 `}
+        className={`transition ease-in-out hover:scale-105 flex flex-1`}
       />
       <BannerInfo />
     </section>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import Banner from "@/components/modules/banner/Banner";
 
 function Index() {
   return (
-    <div className={`max-w-[1280px] m-auto`}>
-      <div className={`flex flex-1 `}>
+    <div className={`flex max-w-[var(--max-container-size)] m-auto`}>
+      <div className={`flex flex-1 max-h-[504px] mt-6`}>
         <Banner />
       </div>
     </div>
