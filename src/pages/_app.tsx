76eaa28ca918aa810b8c8ff6d360615ea8029107
hyunjeen <dev.hyunjeen@gmail.com>
2023-06-02 11:04:32 +0900

    fix: _app 컴포터는 불필요한 코드 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import type { AppContext, AppProps } from "next/app";
+import type { AppProps } from "next/app";
 import { Noto_Sans_KR } from "next/font/google";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import { Provider } from "react-redux";
@@ -9,11 +9,13 @@ import { persistor, store } from "@/store/storeConfig";
 import PagePathMiddleware from "@/middleware/PagePath.middleware";
 import Layout from "@/layout/Layout";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+
 config.autoAddCss = false;
 
 const noToSansKr = Noto_Sans_KR({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
+  display: "swap",
 });
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -40,14 +42,3 @@ export default function App({ Component, pageProps }: AppProps) {
     </Provider>
   );
 }
-App.getInitialProps = async (context: AppContext) => {
-  const { ctx, Component } = context;
-  let pageProps = {};
-
-  if (Component.getInitialProps) {
-    pageProps = (await Component.getInitialProps(ctx)) || {};
-    console.log(pageProps);
-    return { pageProps };
-  }
-  return {};
-};
