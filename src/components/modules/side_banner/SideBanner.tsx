f3fa2d040e99a3de86dd6940d87fe53d26406d52
hyunjeen <dev.hyunjeen@gmail.com>
2023-06-02 20:48:24 +0900

    feature: SideBanner 컴포넌트 완성

--- src/components/modules/side_banner/SideBanner.tsx
+++ src/components/modules/side_banner/SideBanner.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import SwiperCore, { A11y, Autoplay, Navigation, Pagination } from "swiper";
+import Image from "next/image";
+import slide1 from "/public/slide1.jpeg";
+import slide2 from "/public/slide2.jpeg";
+import slide3 from "/public/slide3.jpeg";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+SwiperCore.use([Navigation, Autoplay]);
 
 function SideBanner() {
-  return <div></div>;
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, A11y]}
+      slidesPerView={1}
+      loop
+      navigation
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        el: ".swiper-pagination",
+        type: "custom",
+        renderCustom: (swiper, current, total) => {
+          return `<span}>${current}/${total}</span>`;
+        },
+      }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+      className={`flex basis-[21.875%] ml-4`}
+    >
+      <SwiperSlide>
+        <Image src={slide1} alt={"slide1"} className={`object-cover h-full`} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src={slide2} alt={"slide2"} className={`object-cover h-full`} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src={slide3} alt={"slide3"} className={`object-cover h-full`} />
+      </SwiperSlide>
+      <span className="swiper-pagination w-auto bg-gray-400 rounded-full px-4 bg-black bg-transparent-3 inline bottom-2 right-2 text-white left-auto"></span>
+    </Swiper>
+  );
 }
 
 export default SideBanner;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Banner from "@/components/modules/banner/Banner";
+import SideBanner from "@/components/modules/side_banner/SideBanner";
 
 function Index() {
   return (
     <div className={`flex max-w-[var(--max-container-size)] m-auto`}>
       <div className={`flex flex-1 max-h-[504px] mt-6`}>
         <Banner />
+        <SideBanner />
       </div>
     </div>
   );
