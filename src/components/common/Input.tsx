a22172686acc059ae3f4173af8e21f7636ea32cd
hyunjeen <dev.hyunjeen@gmail.com>
2023-06-02 10:54:15 +0900

    feature: 공통 Input 컴포넌트 다른 라이브러리 호환성 추가

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,13 +1,26 @@
+import { ForwardedRef, forwardRef } from "react";
+
 interface InputProps {
   placeholder: string;
-}
-function Input({ placeholder }: InputProps) {
-  return (
-    <input
-      className={`w-full h-[50px] border rounded px-4 py-3 outline-0`}
-      placeholder={placeholder}
-    />
-  );
+  type?: string;
+  error?: boolean;
 }
 
+const Input = forwardRef(
+  (
+    { placeholder, error, type, ...props }: InputProps,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => (
+    <input
+      ref={ref}
+      type={type}
+      className={`w-full h-[50px] border rounded px-4 py-3 outline-0 ${
+        error ? "border-red-600" : ""
+      }`}
+      placeholder={placeholder}
+      {...props}
+    />
+  )
+);
+Input.displayName = "Input";
 export default Input;
